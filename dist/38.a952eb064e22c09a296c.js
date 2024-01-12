a858fb45b6107b930688a3ebfeef43d39838b80e
inari_senpai <onemorecoffeepls@gmail.com>
2024-01-12 21:40:13 +0100

    fix

--- dist/38.a952eb064e22c09a296c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcalculator=self.webpackChunkcalculator||[]).push([[38],{38:(c,l,e)=>{e.r(l),e.d(l,{default:()=>a});var r=e(893);const a=function(){return(0,r.jsx)("h1",{children:"This is placeholder for help block"})}}}]);
-//# sourceMappingURL=38.a952eb064e22c09a296c.js.map
